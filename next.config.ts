4258e4a4b461e3da49f43575249f1ae837befffc
Rayan-Alaseed <rsyg991@gmail.com>
2026-02-14 22:04:22 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,17 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/-Rayan-sys" : "",
+  assetPrefix: isProd ? "/-Rayan-sys/" : "",
+
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
