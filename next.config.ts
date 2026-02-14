2144d3a23db3b0f23676da3855636766256d34d6
Rayan-Alaseed <rsyg991@gmail.com>
2026-02-14 22:17:03 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,14 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
-output: "export",
-basePath: "/-Rayan-sys",
-assetPrefix: "/-Rayan-sys/",
+
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/-Rayan-sys" : "",
   assetPrefix: isProd ? "/-Rayan-sys/" : "",
 
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    unoptimized: true,
   },
 };
 
