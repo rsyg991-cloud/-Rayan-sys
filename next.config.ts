954f6ccf18dc818c89239a4011d88cbba174cf67
Rayan-Alaseed <rsyg991@gmail.com>
2026-03-13 22:29:07 +0300

    Simplify next.config.ts by removing basePath and assetPrefix
    Remove basePath and assetPrefix for non-production.

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/-Rayan-sys" : "",
-  assetPrefix: isProd ? "/-Rayan-sys/" : "",
 
   images: {
     unoptimized: true,
